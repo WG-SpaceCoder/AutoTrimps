b403a653107ef262b7cb6eb3600e8f27e41346d9
zininzinin <wilgarv@gmail.com>
2016-01-15 15:02:35 -0600

    updateValueFields fixed

--- NewUI.js
+++ NewUI.js
@@ -194,7 +194,7 @@ function autoSetValue(id) {
 function updateValueFields(){
     for (var setting in autoTrimpSettings) {
         if (autoTrimpSettings[setting].type == 'value') {
-            document.getElementById(autoTrimpSettings[setting].id).textContent = autoTrimpSettings[setting].name + ': ' + prettify(autoTrimpSettings[setting].value);
+            document.getElementById(autoTrimpSettings[setting].id).textContent = autoTrimpSettings[setting].name + ': ' + ((autoTrimpSettings[setting].value > 0) ? prettify(autoTrimpSettings[setting].value) : 'Infinite');
         }
     }
 }
