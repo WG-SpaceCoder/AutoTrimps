b025062374d77d2fd3f9f44d957965d9adf0ccff
zininzinin <wilgarv@gmail.com>
2016-01-15 13:14:42 -0600

    Create user.js

---
+++ user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         AutoTrimps
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       zininzinin, spindrjr
+// @include        *trimps.github.io*
+// @grant        none
+// ==/UserScript==
+javascript:with(document)(head.appendChild(createElement('script')).src='https://rawgit.com/zininzinin/AutoTrimps/master/AutoTrimps2.js')._;
