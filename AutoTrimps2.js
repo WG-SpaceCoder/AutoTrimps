e2060d0a6644f2e072a724f7fffbea4232b99218
spindrjr <spindrjr@gmail.com>
2016-01-15 19:27:09 -0700

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -538,7 +538,7 @@ function buyUpgrades() {
         var gameUpgrade = game.upgrades[upgrade];
         var available = (gameUpgrade.allowed > gameUpgrade.done && canAffordTwoLevel(gameUpgrade));
         if (upgrade == 'Coordination' && !canAffordCoordinationTrimps()) continue;
-        if (upgrade == 'Shieldblock' && !getPageSetting(BuyShieldblock)) continue;
+        if (upgrade == 'Shieldblock' && !getPageSetting('BuyShieldblock')) continue;
         if (upgrade == 'Gigastation' && (game.global.lastWarp ? game.buildings.Warpstation.owned < game.global.lastWarp + getPageSetting('DeltaGigastation') : game.buildings.Warpstation.owned < getPageSetting('FirstGigastation'))) continue;
         if ((!game.upgrades.Scientists.done && upgrade != 'Battle') ? (available && upgrade == 'Scientists' && game.upgrades.Scientists.allowed) : (available)) {
             buyUpgrade(upgrade);
