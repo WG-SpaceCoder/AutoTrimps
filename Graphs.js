3e28a693e6ca21da2855f604aa41cf8f356ad574
zininzinin <wilgarv@gmail.com>
2016-01-21 16:36:32 -0600

    VeryWIP
    Loses previous graphed data, but should not in the future.

--- Graphs.js
+++ Graphs.js
@@ -1,23 +1,44 @@
+//Import the Chart Libraries
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://code.highcharts.com/highcharts.js';
+head.appendChild(script);
+
+//Create the graph button and div
 var newItem = document.createElement("TD");
 newItem.appendChild(document.createTextNode("Graphs"));
 newItem.setAttribute("class", "btn btn-default");
 newItem.setAttribute("onclick", "autoToggleGraph()");
 var settingbarRow = document.getElementById("settingsTable").firstElementChild.firstElementChild;
 settingbarRow.insertBefore(newItem, settingbarRow.childNodes[10]);
-document.getElementById("settingsRow").innerHTML += '<div id="graph" style="display: none;margin-bottom: 2vw;margin-top: 2vw;"></div>';
+document.getElementById("settingsRow").innerHTML += '<div id="graphParent" style="display: none;"><div id="graph" style="margin-bottom: 2vw;margin-top: 2vw;"></div></div>';
 
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://code.highcharts.com/highcharts.js';
-head.appendChild(script);
-script.src = 'https://code.highcharts.com/modules/exporting.js';
-head.appendChild(script);
+//Create the dropdown for what graph to show
+var graphList = ['HeliumPerHour', 'Helium', 'Resources'];
+var btn = document.createElement("select");
+btn.id = 'graphSelection';
+btn.setAttribute("style", "color:black");
+btn.setAttribute("class", "settingBtn");
+btn.setAttribute("onmouseover", 'tooltip(\"Graph\", \"customText\", event, \"What graph would you like to display you nerd you?\")');
+btn.setAttribute("onmouseout", 'tooltip("hide")');
+for (var item in graphList) {
+    var option = document.createElement("option");
+    option.value = graphList[item];
+    option.text = graphList[item];
+    btn.appendChild(option);
+}
+document.getElementById('graphParent').appendChild(btn);
+
+
+
+
+
 
 
 function autoToggleGraph() {
     if (game.options.displayed) toggleSettingsMenu();
-    var item = document.getElementById('graph');
+    var item = document.getElementById('graphParent');
     if (item.style.display === 'block') item.style.display = 'none';
     else {
         item.style.display = 'block';
@@ -26,18 +47,18 @@ function autoToggleGraph() {
 }
 
 function autoPlusGraphMenu() {
-    var item = document.getElementById('graph');
+    var item = document.getElementById('graphParent');
     if (item.style.display === 'block') item.style.display = 'none';
     toggleSettingsMenu();
 }
 
-function setGraph() {
+function setGraph(title, xTitle, yTitle, valueSuffix, series) {
     chart1 = new Highcharts.Chart({
         chart: {
             renderTo: 'graph'
         },
         title: {
-            text: 'Helium per Hour',
+            text: title,
             x: -20 //center
         },
         plotOptions: {
@@ -52,12 +73,12 @@ function setGraph() {
         xAxis: {
             floor: 1,
             title: {
-                text: 'Zone'
+                text: xTitle
             },
         },
         yAxis: {
             title: {
-                text: 'Helium/Hour'
+                text: yTitle
             },
             plotLines: [{
                 value: 0,
@@ -66,7 +87,7 @@ function setGraph() {
             }]
         },
         tooltip: {
-            valueSuffix: ' Helium/Hour'
+            valueSuffix: valueSuffix
         },
         legend: {
             layout: 'vertical',
@@ -74,87 +95,139 @@ function setGraph() {
             verticalAlign: 'middle',
             borderWidth: 0
         },
-        series: graphData,
-        loading: {
-            showDuration: 0
-        }
+        series: series
     });
 }
 
 function setColor(tmp) {
-	for (var i in tmp) {
-		if (i == tmp.length-1) {
-			tmp[i].color = '#FF0000' //Current run is in red
-		} else {
-			tmp[i].color = '#90C3D4' //Old runs are in blue
-		}
-	}
-	return tmp;
-}
-
-function addNewWorldToData() {
-    var tmpData = [];
-    var timeThisPortal = new Date().getTime() - game.global.portalTime;
-    timeThisPortal /= 3600000;
-
-    for (var i = 0; i <= game.global.world; i++) {
-        if (i == game.global.world) {
-            tmpData.push(Math.floor(game.resources.helium.owned / timeThisPortal));
+    for (var i in tmp) {
+        if (i == tmp.length - 1) {
+            tmp[i].color = '#FF0000' //Current run is in red
         } else {
-            tmpData.push(0);
+            tmp[i].color = '#90C3D4' //Old runs are in blue
         }
     }
-    graphData.push({
-        name: ('world ' + game.global.totalPortals),
-        data: tmpData
-    });
+    return tmp;
 }
 
+function pushData() {
+    console.log('Pushing Data');
+    allSaveData.push({
+        totalPortals: game.global.totalPortals,
+        heliumOwned: game.resources.helium.owned,
+        currentTime: new Date().getTime(),
+        portalTime: game.global.portalTime,
+        resources: game.resources,
+        world: game.global.world
+    });
+    localStorage.setItem('allSaveData', JSON.stringify(allSaveData));
+}
+
+
 function gatherInfo() {
-    var oldData = JSON.stringify(graphData);
-    var timeThisPortal = new Date().getTime() - game.global.portalTime;
-    timeThisPortal /= 3600000;
-
-    if (graphData === null) {
-    	graphData = [];
+    if (allSaveData === null) {
+        allSaveData = [];
     }
 
-    if (graphData.length === 0) {
-        addNewWorldToData();
-    } else if (graphData[graphData.length - 1].name != ('world ' + game.global.totalPortals)) {
-        addNewWorldToData();
-    } else if (currentZone != game.global.world) {
-        graphData[graphData.length - 1].data.push(Math.floor(game.resources.helium.owned / timeThisPortal));
-        currentZone = game.global.world;
+    if (allSaveData.length === 0) {
+        pushData();
+    } else if (allSaveData[allSaveData.length - 1].world != game.global.world) {
+        pushData();
     }
 
-    if (oldData != JSON.stringify(graphData)) {
-        setGraph();
-    }
+    //Test graph for food
+    setGraphData(document.getElementById('graphSelection').value);
 
-    graphData = setColor(graphData);
-    localStorage.setItem('graphData', JSON.stringify(graphData));
+    // graphData = setColor(graphData);
+
     setTimeout(gatherInfo, 1000);
 }
 
-
-
-
-
-
-
-
-
-var worldZones = [];
-while (worldZones.length < 100) {
-    worldZones.push(worldZones.length);
+function setGraphData(graph) {
+    var title, xTitle, yTitle, valueSuffix, series;
+    var oldData = JSON.stringify(graphData);
+    valueSuffix = '';
+    switch (graph) {
+        case 'Resources':
+            var foodData = {
+                name: 'Food',
+                data: []
+            };
+            var woodData = {
+                name: 'Wood',
+                data: []
+            };
+            var metalData = {
+                name: 'Metal',
+                data: []
+            };
+            for (var i in allSaveData) {
+                if (allSaveData[i].totalPortals == allSaveData[allSaveData.length - 1].totalPortals) {
+                    foodData.data.push(allSaveData[i].resources.food.max);
+                    woodData.data.push(allSaveData[i].resources.wood.max);
+                    metalData.data.push(allSaveData[i].resources.metal.max);
+                }
+            }
+            graphData = [foodData, woodData, metalData];
+            title = 'Max Resources this run';
+            xTitle = 'Zone';
+            yTitle = 'Resources'
+            break;
+        case 'Helium':
+            var currentPortal = -1;
+            graphData = [];
+            for (var i in allSaveData) {
+                if (allSaveData[i].totalPortals != currentPortal) {
+                    graphData.push({
+                        name: 'Portal number ' + allSaveData[i].totalPortals,
+                        data: []
+                    })
+                    currentPortal = allSaveData[i].totalPortals;
+                }
+                graphData[graphData.length - 1].data.push(allSaveData[i].heliumOwned);
+            }
+            title = 'Helium';
+            xTitle = 'Zone';
+            yTitle = 'Helium'
+            break;
+        case 'HeliumPerHour':
+            var currentPortal = -1;
+            graphData = [];
+            for (var i in allSaveData) {
+                if (allSaveData[i].totalPortals != currentPortal) {
+                    graphData.push({
+                        name: 'Portal number ' + allSaveData[i].totalPortals,
+                        data: []
+                    })
+                    currentPortal = allSaveData[i].totalPortals;
+                }
+                graphData[graphData.length - 1].data.push(Math.floor(allSaveData[i].heliumOwned / ((allSaveData[i].currentTime - allSaveData[i].portalTime) / 3600000)));
+            }
+            title = 'Helium/Hour';
+            xTitle = 'Zone';
+            yTitle = 'Helium'
+            break;
+    }
+    if (oldData != JSON.stringify(graphData)) {
+        setGraph(title, xTitle, yTitle, valueSuffix, graphData);
+    }
 }
+
+
+
+
+
+
+
+
+
+var allSaveData = [];
 var graphData = [];
-var tmpGraphData = JSON.parse(localStorage.getItem('graphData'));
+var tmpGraphData = JSON.parse(localStorage.getItem('allSaveData'));
 if (tmpGraphData !== null) {
-	console.log('got graphData yay');
-    graphData = tmpGraphData;
+    console.log('Got allSaveData. Yay!');
+    allSaveData = tmpGraphData;
 }
-var currentZone = game.global.world;
+
 
 setTimeout(gatherInfo, 1000);
