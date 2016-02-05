f2924cba80435e4fc1678b7b674ea97a5d8218df
spindrjr <spindrjr@gmail.com>
2016-02-05 07:29:20 -0700

    highcharts is dumb

--- Graphs.js
+++ Graphs.js
@@ -2,7 +2,7 @@
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://github.highcharts.com/highcharts.src.js';
+script.src = 'https://code.highcharts.com/highcharts.js';
 head.appendChild(script);
 
 //Create the graph button and div
