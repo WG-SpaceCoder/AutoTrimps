45adc2e613dfcdcb692cf0bcf4034ba7808db15e
spindrjr <spindrjr@gmail.com>
2016-02-22 17:54:50 -0700

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -835,7 +835,7 @@ function manualLabor() {
     else if (breedingTrimps < 10 && game.buildings.Trap.owned > 0) {
     	setGather('trimps');
     }
-   else if (game.resources.science.owned < 100) setGather('science');
+   else if (game.resources.science.owned < 100 && document.getElementById('scienceCollectBtn').style.display == 'block') setGather('science');
     //if we have more than 2 buildings in queue, or (our modifier is real fast and trapstorm is off), build
    else if (game.global.buildingsQueue.length ? (game.global.buildingsQueue.length > 1 || game.global.autoCraftModifier == 0 || (game.global.playerModifier > 1000 && game.global.trapBuildToggled == false)) : false) {
         // debug('Gathering buildings??');
