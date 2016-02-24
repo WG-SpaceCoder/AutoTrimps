c69275458390e3bb8f7feac319af080f4dc42a07
Mori <moriakaice@gmail.com>
2016-02-24 14:13:53 +0100

    Support Kongregate
    Add Kongregate to the list of pages for include, so the script is also included there

--- user.js
+++ user.js
@@ -5,6 +5,7 @@
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru
 // @include        *trimps.github.io*
+// @include        *kongregate.com/games/GreenSatellite/trimps
 // @grant        none
 // ==/UserScript==
 javascript:with(document)(head.appendChild(createElement('script')).src='https://zininzinin.github.io/AutoTrimps/AutoTrimps2.js')._;
