341921369772772a3b5e43bddcdf728f2c50a531
spindrjr <spindrjr@gmail.com>
2016-04-28 18:08:37 -0600

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -2008,6 +2008,7 @@ setTimeout(delayStart, 2000);
 function mainLoop() {
 	game.global.addonUser = true;
 	if(getPageSetting('PauseScript')) return;
+	if(game.global.viewingUpgrades) return;
     setTitle();
     setScienceNeeded();
     updateValueFields();
