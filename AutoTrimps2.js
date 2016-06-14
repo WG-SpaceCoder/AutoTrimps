d4c95799160e991590c97cad69b147c6d95410dc
gen BTC <genBTC@hushmail.com>
2016-06-13 23:45:45 -0400

    Tab to spaces :ugh:

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -198,24 +198,24 @@ function timeStamp() {
 function getPerSecBeforeManual(job) {
     var perSec = 0;
     if (game.jobs[job].owned > 0){
-		perSec = (game.jobs[job].owned * game.jobs[job].modifier);
-		if (game.portal.Motivation.level > 0) perSec += (perSec * game.portal.Motivation.level * game.portal.Motivation.modifier);
-		if (game.portal.Motivation_II.level > 0) perSec *= (1 + (game.portal.Motivation_II.level * game.portal.Motivation_II.modifier));
-		if (game.portal.Meditation.level > 0) perSec *= (1 + (game.portal.Meditation.getBonusPercent() * 0.01)).toFixed(2);
-		if (game.global.challengeActive == "Meditate") perSec *= 1.25;
-		else if (game.global.challengeActive == "Size") perSec *= 1.5;
-		if (game.global.challengeActive == "Toxicity"){
-			var toxMult = (game.challenges.Toxicity.lootMult * game.challenges.Toxicity.stacks) / 100;
-			perSec *= (1 + toxMult);
-		}
-		if (game.global.challengeActive == "Balance"){
-			perSec *= game.challenges.Balance.getGatherMult();
-		}
-		if (game.global.challengeActive == "Watch") perSec /= 2;
-		if (game.global.challengeActive == "Lead" && ((game.global.world % 2) == 1)) perSec*= 2;
-		perSec = calcHeirloomBonus("Staff", job + "Speed", perSec);
-	}
-	return perSec
+        perSec = (game.jobs[job].owned * game.jobs[job].modifier);
+        if (game.portal.Motivation.level > 0) perSec += (perSec * game.portal.Motivation.level * game.portal.Motivation.modifier);
+        if (game.portal.Motivation_II.level > 0) perSec *= (1 + (game.portal.Motivation_II.level * game.portal.Motivation_II.modifier));
+        if (game.portal.Meditation.level > 0) perSec *= (1 + (game.portal.Meditation.getBonusPercent() * 0.01)).toFixed(2);
+        if (game.global.challengeActive == "Meditate") perSec *= 1.25;
+        else if (game.global.challengeActive == "Size") perSec *= 1.5;
+        if (game.global.challengeActive == "Toxicity"){
+            var toxMult = (game.challenges.Toxicity.lootMult * game.challenges.Toxicity.stacks) / 100;
+            perSec *= (1 + toxMult);
+        }
+        if (game.global.challengeActive == "Balance"){
+            perSec *= game.challenges.Balance.getGatherMult();
+        }
+        if (game.global.challengeActive == "Watch") perSec /= 2;
+        if (game.global.challengeActive == "Lead" && ((game.global.world % 2) == 1)) perSec*= 2;
+        perSec = calcHeirloomBonus("Staff", job + "Speed", perSec);
+    }
+    return perSec
 }
 
 //Called before buying things that can be purchased in bulk
